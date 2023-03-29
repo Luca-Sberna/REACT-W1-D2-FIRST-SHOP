42b74b4fb7578b546fdbe941dacb7248b0ad9d0f
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-03-29 17:59:25 +0200

    adj

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { Container, Card, Button, Row } from "react-bootstrap";
+import { Container, Card, Button, Row, Col } from "react-bootstrap";
 import fantasy from "../data/books/fantasy.json";
 import history from "../data/books/history.json";
 import horror from "../data/books/horror.json";
@@ -23,29 +23,29 @@ class AllTheBooks extends Component {
     allTheBooks.sort(this.getRandomNumber);
 
     return (
-      <Container className="py-5 justify-content-center border bg-primary rounded-4">
-        <Row className="gap-3 justify-content-center">
+      <Container className="py-5 border bg-primary rounded-4 ">
+        <Row className="gap-3 justify-content-center ">
           {allTheBooks.map((book, index) => (
-            <Card
-              style={{ width: "19.19rem" }}
-              key={`book-${index}`}
-              className="p-2"
-            >
-              <Card.Img
-                variant="top"
-                src={book.img}
-                className=" rounded"
-                style={{ height: "25rem" }}
-              />
-              <Card.Body className="px-1">
-                <Card.Title>{book.title}</Card.Title>
-                <Card.Text>{book.price + "$"}</Card.Text>
-                <Card.Text>{book.category}</Card.Text>
-              </Card.Body>
-              <Button className="w-25" variant="success">
-                Add
-              </Button>
-            </Card>
+            <Col className="p-1" sx={12} md={5} lg={3} xl={2}>
+              <Card key={`book-${index}`} className="p-2">
+                <Card.Img
+                  variant="top"
+                  src={book.img}
+                  className=" rounded"
+                  style={{ height: "22rem" }}
+                />
+                <Card.Body className="px-1">
+                  <Card.Title className="text-truncate">
+                    {book.title}
+                  </Card.Title>
+                  <Card.Text>{book.price + "$"}</Card.Text>
+                  <Card.Text>{book.category}</Card.Text>
+                </Card.Body>
+                <Button className="w-50" variant="success">
+                  Add
+                </Button>
+              </Card>
+            </Col>
           ))}
           ;
         </Row>
